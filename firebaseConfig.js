e3d576902a237cbc1e1302d0935f785473a87d3d
xexter <madhavpolawar2012@gmail.com>
2024-08-09 19:20:01 +0530

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsbLRU-F7YRPi7hB7_TVXIv58QmEGvCmY",
+  authDomain: "food-hunt-257df.firebaseapp.com",
+  projectId: "food-hunt-257df",
+  storageBucket: "food-hunt-257df.appspot.com",
+  messagingSenderId: "741131916421",
+  appId: "1:741131916421:web:2e8251a87d98272a3ec5ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+      <div className='container-max text-center py-4 border-t'>
+        Foody, made by{' '}
+        <a
+          href='https://foodhunt-geekster.netlify.app'
+          target={'_blank'}
+          className='text-orange-500'
+        >
+          Madhav Polawar
+        </a>
+      </div>
+    );
+  };
+  export default Footer;
